3ea5935d10da39fe7022afd08b269bb1a1de72ec
Harsh <rajh87897@gmail.com>
2025-11-07 09:34:30 +0530

    Project v1.0

---
+++ travel-backend-main/travel-backend-main/src/main/java/com/klu/travelmanagement/TravelmanagementApplication.java
@@ -0,0 +1,14 @@
+package com.klu.travelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelmanagementApplication.class, args);
+		System.out.println("Travel Management Backend Started");
+	}
+
+}
